ce700f99d8abc3e02b817880e698ea4a72963009
ingrid <ingridjmartinez@gmail.com>
2025-07-18 09:32:53 -0300

    Se modifico contacto

--- contacto.php
+++ contacto.php
@@ -8,7 +8,7 @@
 </head>
 <body>
   <h1>¡Hola, Mundo!</h1>
-  <p>Este es un párrafo de ejemplo. Aquí puedes agregar más contenido a tu página.</p>
+  <p>Este es un párrafo de ejemplo. Aquí puedes agregar más contenido a tu página. INGRID</p>
   <a href="https://www.ejemplo.com">Enlace a un sitio web</a>
   <img src="imagen.jpg" alt="Descripción de la imagen">
 </body>
