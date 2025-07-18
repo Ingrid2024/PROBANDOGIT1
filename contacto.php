c5a15ae08c5715da1e6b907f26663c4a5870ae5d
ingrid <ingridjmartinez@gmail.com>
2025-07-18 08:48:29 -0300

    VEMOS

---
+++ contacto.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mi Primera Página para git</title>
+</head>
+<body>
+  <h1>¡Hola, Mundo!</h1>
+  <p>Este es un párrafo de ejemplo. Aquí puedes agregar más contenido a tu página.</p>
+  <a href="https://www.ejemplo.com">Enlace a un sitio web</a>
+  <img src="imagen.jpg" alt="Descripción de la imagen">
+</body>
+</html>
